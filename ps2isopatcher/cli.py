247377ba28fc496836e2a7b8fe21495d13e88d25
Jasper Chan <jasperchan515@gmail.com>
2020-06-28 18:36:00 -0700

    Allow simple replacement of files

--- ps2isopatcher/cli.py
+++ ps2isopatcher/cli.py
@@ -1,12 +1,41 @@
-from ps2isopatcher.iso import Ps2Iso
+from bitstring import Bits
+from ps2isopatcher.iso import Ps2Iso, TreeFolder
+
+def print_tree(item, level=0):
+    name = item.name
+    spacing = " "*(level*2)
+    print(f"{spacing}{name}")
+    if isinstance(item, TreeFolder):
+        for c in item.children:
+            print_tree(c, level=level+1)
+
+def print_tree_flat(item):
+    path = item.path
+    print(path)
+    if isinstance(item, TreeFolder):
+        for c in item.children:
+            print_tree_flat(c)
 
 
 def main():
     f_name = "mm.iso"
     iso = Ps2Iso(f_name)
-    print(iso.pvd.l_path_table)
-    for dir in iso.path_tables.l_path_table.get_paths():
-        print(dir)
+    print_tree(iso.tree)
+    print_tree_flat(iso.tree)
+    path = "/RAW/IOPRP310.IMG;1"
+    print(iso.get_object(path))
+    print(iso.get_object(path).name)
+    print(iso.get_object(path).lba)
+    print(f"{path} has {iso.get_blocks_allocated(path)} blocks allocated")
+    replacements = [(path, Bits(filename="test.txt"))]
+    print(f"Replacing {path} with test.txt")
+    import pdb
+    pdb.set_trace()
+    iso.replace_files(replacements)
+    print(f"Writing to out.iso")
+    import pdb
+    pdb.set_trace()
+    iso.write("out.iso")
 
 
 if __name__ == "__main__":
--- ps2isopatcher/iso.py
+++ ps2isopatcher/iso.py
@@ -1,5 +1,6 @@
 import logging
-from bitstring import Bits
+from math import ceil
+from bitstring import BitArray, Bits
 
 
 class PVD:
@@ -30,12 +31,12 @@ class PVD:
     def logical_block_size(self):
         return self._get_intle(
             self.BLOCK_SIZE_OFFSET, self.BLOCK_SIZE_LENGTH)
-    
+
     @property
     def path_table_size(self):
         return self._get_intle(
             self.PATH_TABLE_SIZE_OFFSET, self.PATH_TABLE_SIZE_LENGTH)
-    
+
     @property
     def l_path_table(self):
         return self._get_intle(
@@ -54,7 +55,7 @@ class PVD:
     def m_path_table_opt(self):
         return self._get_intle(
             self.M_PATH_TABLE_OPT_OFFSET, self.PATH_TABLE_LENGTH)
-    
+
     def _get_intle(self, offset, length):
         e = self._get_entry(offset, length)
         return e.intle
@@ -62,7 +63,7 @@ class PVD:
     def _get_intme(self, offset, length):
         e = self._get_entry(offset, length)
         return e.intme
-    
+
     def _get_str(self, offset, length):
         e = self._get_entry(offset, length)
         return e.tobytes().decode().strip()
@@ -71,21 +72,22 @@ class PVD:
         return self.data[offset*8:(offset+length)*8]
 
 class PathTable:
-    def __init__(self, data):
+    def __init__(self, data, addr, size):
         self.data = data
-        self.length = len(data)/8
-    
-    def get_paths(self):
+        self.tbl_data = self.data[addr:(addr+size)]
+        self.length = len(self.tbl_data)/8
+
+    def get_entries(self):
         paths = []
 
         i = 0
         dir_id = 1
         while i < self.length:
-            name_len = self.data[i*8:(i+1)*8].int
+            name_len = self.tbl_data[i*8:(i+1)*8].int
             total_len = name_len + 8
             if name_len % 2:
                 total_len += 1
-            entry = self.data[i*8:(i+total_len)*8]
+            entry = self.tbl_data[i*8:(i+total_len)*8]
             lba = self._get_lba(entry)
             parent_dir_id = self._get_parent_dir_id(entry)
             name = self._get_name(entry, name_len)
@@ -98,13 +100,13 @@ class PathTable:
             i += total_len
             dir_id += 1
         return paths
-    
+
     def _get_name(self, entry, length):
         return entry[8*8:(8+length)*8].tobytes().decode().strip()
 
     def _get_lba(self, entry):
         pass
-    
+
     def _get_parent_dir_id(self, entry):
         pass
 
@@ -118,18 +120,169 @@ class LPathTable(PathTable):
 
 class PathTables:
     def __init__(self, data, pvd):
+        self.data = data
         size = pvd.path_table_size*8
         lpt_addr = pvd.l_path_table*2048*8
-        self.l_path_table = LPathTable(data[lpt_addr:(lpt_addr+size)])
+        self.l_path_table = LPathTable(self.data, lpt_addr, size)
+
+    def get_path_tree(self):
+        paths = self.l_path_table.get_entries()
+        root = paths.pop(0)
+        return TreeFolder(root, children=paths, data=self.data)
+
+class DirTable:
+    def __init__(self, data, lba, block_size):
+        self.dt_addr = lba*block_size*8
+        self.dt_size = block_size*8
+        self.data = data
+        self.set_tbl_data()
+
+    def get_entries(self):
+        entries = []
+
+        i = 0
+        while True:
+            total_len = self.tbl_data[i*8:(i+1)*8].int
+            if total_len == 0:
+                break
+            entry = self.tbl_data[i*8:(i+total_len)*8]
+            lba = self._get_lba(entry)
+            size = self._get_size(entry)
+            name_len = self._get_name_length(entry)
+            name = self._get_name(entry, name_len)
+            entries.append({
+                "name": name,
+                "size": size,
+                "lba": lba,
+            })
+            i += total_len
+        return entries
+
+    def set_entry(self, name, lba, size):
+        i = 0
+        print(f"Searching for {name}")
+        while True:
+            total_len = self.tbl_data[i*8:(i+1)*8].int
+            if total_len == 0:
+                break
+            offset = i*8
+            entry = self.tbl_data[offset:(i+total_len)*8]
+            name_len = self._get_name_length(entry)
+            n = self._get_name(entry, name_len)
+            print(n)
+            if n == name:
+                iso_offset = offset + self.dt_addr
+                lba_offset = iso_offset + 2*8
+                size_offset = iso_offset + 10*8
+                lba_le = Bits(uintle=lba, length=4*8)
+                lba_be = Bits(uintbe=lba, length=4*8)
+                lba_bits = BitArray()
+                lba_bits.append(lba_le)
+                lba_bits.append(lba_be)
+                size_le = Bits(uintle=size, length=4*8)
+                size_be = Bits(uintbe=size, length=4*8)
+                size_bits = BitArray()
+                size_bits.append(size_le)
+                size_bits.append(size_be)
+                self.data.overwrite(lba_bits, lba_offset)
+                self.data.overwrite(size_bits, size_offset)
+                import pdb
+                pdb.set_trace()
+                self.set_tbl_data()
+                break
+            i += total_len
+
+    def set_tbl_data(self):
+        self.tbl_data = self.data[self.dt_addr:(self.dt_addr+self.dt_size)]
+
+    def _get_lba(self, entry):
+        return entry[2*8:6*8].intle
+
+    def _get_size(self, entry):
+        return entry[10*8:14*8].intle
+
+    def _get_name_length(self, entry):
+        return entry[32*8:33*8].intle
+
+    def _get_name(self, entry, length):
+        return entry[33*8:(33+length)*8].tobytes().decode().strip()
+
+
+class TreeObject:
+    def __init__(self, info, parent=None):
+        self.__name = info["name"]
+        self.__lba = info["lba"]
+        self.__parent = parent
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def lba(self):
+        return self.__lba
+
+    @property
+    def path(self):
+        if self.parent is None:
+            return ""
+        return f"{self.parent.path}/{self.name}"
+
+    def update_toc(self, lba, size):
+        self.parent._dirtable.set_entry(self.name, lba, size)
+
+class TreeFile(TreeObject):
+    def __init__(self, info, parent=None):
+        super().__init__(info, parent=parent)
+        self.__size = info["size"]
+
+    @property
+    def size(self):
+        return self.__size
+
+class TreeFolder(TreeObject):
+    def __init__(
+            self, info, parent=None, children=None, data=None, block_size=2048):
+        super().__init__(info, parent=parent)
+        self.id = info["dir_id"]
+        self.__children = []
+        if children:
+            direct_children = list(filter(
+                lambda x: x["parent_dir_id"] == self.id, children))
+            for c in direct_children:
+                children.remove(c)
+            for c in direct_children:
+                child = TreeFolder(c, parent=self, children=children, data=data)
+                self.__children.append(child)
+        self._dirtable = DirTable(data, self.lba, block_size)
+        file_entries = self._dirtable.get_entries()
+
+        files = []
+        for entry in file_entries:
+            files.append(TreeFile(entry, parent=self))
+        files = files[2:]
+        self.__children.extend(files)
+
+    def get_child(self, name):
+        return next(i for i in self.children if i.name == name)
+
+    @property
+    def children(self):
+        return self.__children
 
 
 class Ps2Iso:
     def __init__(self, filename):
         self._set_logger()
-        self.data = Bits(filename=filename)
+        self.log.info(f"Loading {filename}, this may take a while...")
+        #self.data = Bits(filename=filename)
+        self.data = BitArray(filename=filename)
         self.pvd = PVD(self.data)
         self.block_size = self.pvd.logical_block_size
-        self.path_tables = PathTables(self.data, self.pvd)
 
         if self.pvd.system_identifier != "PLAYSTATION":
             self.log.warning((
@@ -137,24 +290,125 @@ class Ps2Iso:
                 "should be 'PLAYSTATION'"))
             self.log.warning(
                 f"{filename} may not be a PS2 ISO file")
-        if self.block_size != 2048:        
+        if self.block_size != 2048:
             self.log.warning((
                 f"logical_block_size: {self.block_size}, "
                 "should be 2048"))
             self.log.warning(
                 f"{filename} may not be a PS2 ISO file")
-    
+
+        self.path_tables = PathTables(self.data, self.pvd)
+        self.tree = self.path_tables.get_path_tree()
+
+    def get_object(self, path):
+        paths = path.split("/")
+        if paths[0] == "":
+            paths.pop(0)
+        mark = self.tree
+        for p in paths:
+            mark = mark.get_child(p)
+        return mark
+
+    def get_blocks_allocated(self, path):
+        obj = self.get_object(path)
+        lba_list = self.get_lba_list()
+        obj_idx = next(idx for idx, i in enumerate(lba_list) if i[1] == path)
+        lba = lba_list[obj_idx][0]
+        next_lba = lba_list[obj_idx + 1][0]
+        return next_lba - lba
+
+    def get_lba(self, path):
+        return self.get_object(path).lba
+
+    def replace_files(self, replacements, allow_move=False):
+        paths = [path for path, _ in replacements]
+        bins = [b for _, b in replacements]
+        sizes = [len(b)//8 for b in bins]
+        blocks_required = [ceil(len(b)/8/self.block_size) for b in bins]
+        curr_lba = [self.get_lba(p) for p in paths]
+        curr_blocks_allocated = [self.get_blocks_allocated(p) for p in paths]
+
+        items = [
+            {
+                "path": p,
+                "bin": b,
+                "size": s,
+                "blocks_required": br,
+                "curr_lba": cl,
+                "curr_blocks_allocated": cb
+            } for p, b, s, br, cl, cb in
+            zip(paths, bins, sizes, blocks_required, curr_lba,
+                curr_blocks_allocated)]
+
+        overflows = []
+        for i in items:
+            if i["blocks_required"] > i["curr_blocks_allocated"]:
+                overflows.append(i)
+        for o in overflows:
+            self.log.warning((
+                f"{o['path']} (size: {o['size']} "
+                f"requires {o['blocks_required']} blocks, "
+                f"{o['curr_blocks_allocated']} available"))
+
+        if overflows and not allow_move:
+            raise ValueError("allow_move must be true to increase file sizes")
+
+        for i in items:
+            lba = i["curr_lba"]
+            num_blocks =  i["curr_blocks_allocated"]
+            self.clear_blocks(lba, num_blocks)
+        if not allow_move:
+            for i in items:
+                i["new_lba"] = i["curr_lba"]
+                b = i["bin"]
+                offset = i["curr_lba"]*self.block_size*8
+                self.data.overwrite(b, offset)
+        else:
+            raise NotImplementedError("Moving files is not supported yet")
+
+        for i in items:
+            self.update_toc(i["path"], i["new_lba"], i["size"])
+
+    def update_toc(self, path, lba, size):
+        self.get_object(path).update_toc(lba, size)
+
+    def write(self, filename):
+        with open(filename, "wb") as f:
+            self.data.tofile(f)
+
+    def clear_blocks(self, start_block, num_blocks):
+        start_addr = start_block*self.block_size*8
+        end_addr = start_addr + num_blocks*self.block_size*8
+        self.data.set(0, range(start_addr, end_addr))
+
+    def get_lba_list(self):
+        root = self.tree
+        lba_list = self._get_lba_list(root)
+        lba_list = list(set(lba_list))
+        return sorted(lba_list, key=lambda x: x[0])
+
+    def _get_lba_list(self, item, lba_list=None):
+        if lba_list is None:
+            lba_list = []
+        lba = item.lba
+        path = item.path
+        lba_list.append((lba, path))
+        if isinstance(item, TreeFolder):
+            for c in item.children:
+                self._get_lba_list(c,lba_list=lba_list)
+        return lba_list
 
     def _get_blocks(self, lba, blocks=None, size=None):
         if blocks and size is None:
             size = blocks*self.block_size
         if size is None:
             raise ValueError("blocks/size must be set")
-        
-    
+
     def _set_logger(self):
         self.log = logging.getLogger("Ps2Iso")
         handler = logging.StreamHandler()
         formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
         self.log.addHandler(handler)
+        self.log.setLevel(logging.INFO)
+
