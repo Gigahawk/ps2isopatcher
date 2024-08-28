98a08a4e6add4134f286e0df396effd549f1c0f9
Jasper Chan <jasperchan515@gmail.com>
2024-08-28 03:38:26 +0000

    chore: support building with flake

--- flake.nix
+++ flake.nix
@@ -4,18 +4,37 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv defaultPoetryOverrides;
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
+        mkPoetryEnv mkPoetryApplication defaultPoetryOverrides;
       #poetryEnv = mkPoetryEnv {
       #  projectDir = ./.;
       #};
     in {
+      packages = {
+        ps2isopatcher = mkPoetryApplication {
+          projectDir = self;
+          overrides = defaultPoetryOverrides.extend
+            (final: prev: {
+              hatchling = prev.hatchling.overridePythonAttrs
+              (
+                old: {
+                  buildInputs = (old.buildInputs or [ ]) ++ [ prev.pluggy ];
+                }
+              );
+            });
+        };
+        default = self.packages.${system}.ps2isopatcher;
+      };
       #devShells.default = pkgs.mkShell {
       #  buildInputs = [
       #    poetryEnv
